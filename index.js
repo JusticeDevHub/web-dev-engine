41a02bd38e2f7a18c377eb9f9c1d89e5f14a1d64
Justice Kawuma <justicedevhub@gmail.com>
2024-06-15 15:59:19 +0800

    created test function logger

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Basic Logger for Testing Reason
+ *
+ * @param {string} msg - Message that will be logged into console
+ */
+const Log = (msg) => {
+  console.log(msg);
+};
+
+export default Log;
